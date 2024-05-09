72d9eb6b3890f7778bf277d5b7a7b4e26857c573
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-09 14:05:00 +0300

    fixed Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/Navbar.css";
 
-<<<<<<< HEAD
 const NavBar = () => {
   return (
     <nav className="navbar">  {/* Apply the "navbar" class directly to the nav element */}
@@ -24,26 +23,4 @@ const NavBar = () => {
     </nav>
   );
 };
-=======
-    const NavBar = () => {
-        return (
-            <div className="navbar"> {/* Apply the "navbar" class */}
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/" className="navlink">Home</Link> {/* Apply the "navlink" class */}
-                        </li>
-                        <li>
-                            <Link to="/forecast" className="navlink">Forecast</Link> {/* Apply the "navlink" class */}
-                        </li>
-                        <li>
-                            <Link to="/airpollution" className="navlink">Air Quality</Link> {/* Apply the "navlink" class */}
-                        </li>
-                    </ul>
-                </nav> 
-            </div>
-        );
-    };
->>>>>>> d7bec8d7ca8207d18b0a64f6bf3add876207d1d6
-
 export default NavBar;
