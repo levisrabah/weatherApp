39b6eb3e1ded8e688cf3d49ad7e723c1d59cca63
levisrabah <levisrabah@gmail.com>
2024-05-18 13:57:55 +0300

    final commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import "../App.css";
 import Weather from '../components/Weather';
 import NavBar from "../components/NavBar";
 import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
